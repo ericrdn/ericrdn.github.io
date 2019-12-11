4455dad277ef95cfbdfe98361e2fc1b8e7a94170
Eric <eric.rdn@gmail.com>
2019-12-10 21:36:02 -0300

    Versao 1

---
+++ precache-manifest.b770c0ff36bfc42ab8cb0368c39a871f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d72f70f4422ace8bb88508b4f3929fad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "921d86aca472683a6626",
+    "url": "/static/css/main.49c63680.chunk.css"
+  },
+  {
+    "revision": "97000f05a5294f287a9c",
+    "url": "/static/js/2.ae2533e3.chunk.js"
+  },
+  {
+    "revision": "921d86aca472683a6626",
+    "url": "/static/js/main.6644c77d.chunk.js"
+  },
+  {
+    "revision": "89358667f0c5a6a7ccc1",
+    "url": "/static/js/runtime-main.6a5e950c.js"
+  }
+]);
